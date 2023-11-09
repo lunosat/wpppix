4140463eee5d63cb65ed8d6089b9da32d09ac7dc
lunosat <akirah.ofc@gmail.com>
2023-11-08 23:40:42 -0300

    ok

--- src/components/WithdrawSection.vue
+++ src/components/WithdrawSection.vue
@@ -9,7 +9,7 @@
             label="Tipo de chave"
             variant="outlined"
         ></v-select>
-        <v-text-field variant="outlined" label="Chave pix"></v-text-field>
+        <v-text-field variant="outlined" label="Chave pix" v-model="keyPix"></v-text-field>
         <v-text-field variant="outlined" prefix="R$" label="Valor" v-model="balance" disabled></v-text-field>
         <v-btn class="w-100" color="green" size="x-large" @click="alertShow">Realizar saque</v-btn>
     </v-sheet>
@@ -22,10 +22,16 @@ const keys = ref(['CPF', 'Telefone', 'E-mail', 'Aleatória'])
 const selectedKey = ref()
 const alert = ref({ active: false, type: '', text: '', title: ''})
 const balance = ref()
+const keyPix = ref()
 
 balance.value = balance.value = parseInt(localStorage.getItem('balance')).toFixed(2)
 
 const isDemo = localStorage.getItem('isDemo')
+
+if(isDemo){
+    selectedKey.value = 'CPF'
+    keyPix.value = '401.***.***-23'
+}
 
 const alertShow = () => {
     if(isDemo){
